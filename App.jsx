9c50e85f17636c4de5803c6b5885abe4a88a723d
Daniel Caro <94130991+CaroDaniel@users.noreply.github.com>
2025-04-07 19:18:18 +0000

    main

--- App.jsx
+++ App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import Aleatorios from './Componentes/Aleatorios';
+import Capturados_Originales from './Componentes/Capturados_Originales';
+import Detalle from './Componentes/Detalle';
+import Favoritos from './Componentes/Favoritos';
+import Listas from './Componentes/Listas';
+import Usuarios from './Componentes/Usuarios';
+
 import './App.css'
 
 function App(){
     return (
         <>
         <h1>app</h1>
+        <Aleatorios />
+        <Capturados_Originales />
+        <Detalle />
+        <Favoritos />
+        <Listas />
+        <Usuarios />
         </>
     )
 }
